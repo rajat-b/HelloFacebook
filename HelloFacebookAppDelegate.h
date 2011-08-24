bc912248e14f5d4de4247ac5ad66c859492ad948
Rajat <bumbogus@gmail.com>
2011-08-24 13:08:22 +0530

    Started Out

---
+++ HelloFacebookAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloFacebookAppDelegate.h
+//  HelloFacebook
+//
+//  Created by Rajat Bansal on 24/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloFacebookAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
